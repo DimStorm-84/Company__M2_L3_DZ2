96f357d1173adc9de782b95e330a469efdf1df00
Dima <motevich.dmitrij@gmail.com>
2021-11-09 15:21:18 +0300

    M2 L5 punkt-4

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -3,11 +3,19 @@ import PropTypes from "prop-types";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import User from "./user";
+import api from "../api";
+import GroupList from "./groupList";
+
 const Users = ({ users: allUsers, ...rest }) => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [professions] = useState(api.professions.fetchAll());
     const count = allUsers.length;
     const pageSize = 4;
 
+    const handleProfessionSelect = (params) => {
+        console.log(params);
+    };
+    console.log(professions);
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
         console.log("page: ", pageIndex);
@@ -15,6 +23,10 @@ const Users = ({ users: allUsers, ...rest }) => {
     const usersCrop = paginate(allUsers, currentPage, pageSize);
     return (
         <>
+            <GroupList
+                items={professions}
+                onItemSelect={handleProfessionSelect}
+            />
             {count > 0 && (
                 <table className="table">
                     <thead>
