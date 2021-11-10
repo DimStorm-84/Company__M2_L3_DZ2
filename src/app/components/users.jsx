57873f9a17a3bcc9f3576cf5fe9924f144c3f11b
Dima <motevich.dmitrij@gmail.com>
2021-11-10 15:20:01 +0300

    M2 L5 do p10

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,32 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import User from "./user";
 import api from "../api";
 import GroupList from "./groupList";
-
 const Users = ({ users: allUsers, ...rest }) => {
     const [currentPage, setCurrentPage] = useState(1);
-    const [professions] = useState(api.professions.fetchAll());
+    const [professions, setProfession] = useState();
+    const [selectedProf, setSelectedProf] = useState();
     const count = allUsers.length;
     const pageSize = 4;
+    useEffect(() => {
+        api.professions
+            .fetchAll()
+            .then((data) => setProfession(Object.assign(data)));
+    }, []);
 
-    const handleProfessionSelect = (params) => {
-        console.log(params);
+    const handleProfessionSelect = (item) => {
+        setSelectedProf(item);
     };
-    console.log(professions);
+
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
-        console.log("page: ", pageIndex);
     };
-    const usersCrop = paginate(allUsers, currentPage, pageSize);
+    const filteredUsers = selectedProf
+        ? allUsers.filter((user) => user.profession === selectedProf)
+        : allUsers;
+    const usersCrop = paginate(filteredUsers, currentPage, pageSize);
+    const clearFilter = () => {
+        setSelectedProf();
+    };
     return (
         <>
-            <GroupList
-                items={professions}
-                onItemSelect={handleProfessionSelect}
-            />
+            {professions && (
+                <>
+                    <GroupList
+                        selectedItem={selectedProf}
+                        items={professions}
+                        onItemSelect={handleProfessionSelect}
+                        valueProperty="_id"
+                        contentProperty="name"
+                    />
+                    <button
+                        className="btn btn-secondary mt-2"
+                        onClick={clearFilter}
+                    >
+                        {" "}
+                        Очистить{" "}
+                    </button>
+                </>
+            )}
+
             {count > 0 && (
                 <table className="table">
                     <thead>
